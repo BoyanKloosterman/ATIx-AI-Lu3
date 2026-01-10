40cb470794b919a08ded5e7b50b0cc7ced2e3c79
TjeerdEpic <76160910+TjeerdKootwijk@users.noreply.github.com>
2026-01-10 14:50:01 +0100

    added frontend layout for profile info

--- frontend/src/features/dashboard/pages/Dashboard.tsx
+++ frontend/src/features/dashboard/pages/Dashboard.tsx
@@ -78,9 +78,38 @@ export default function Dashboard({
 
     return (
         <div className="min-h-screen bg-neutral-950 w-full overflow-x-hidden">
-            <div className="max-w-6xl mx-auto px-4 py-8">
+            <div className="max-w-6xl mx-auto px-10 py-8 ">
                 <h1 className="text-4xl font-bold text-white mb-8 text-center">{t.dashboard.title}</h1>
 
+                <div className="bg-gray-800 rounded-lg p-6 mb-15">
+                    <h2 className="text-2xl font-bold text-white mb-2 text-center">
+                        Mijn Profiel
+                    </h2>
+                    
+                    <h3 className="text-xl text-white">Naam</h3>
+                    <p className='mb-2'>PLuk</p>
+                    <h3 className="text-xl text-white">Opleiding</h3>
+                    <p className='mb-2'>Informatica</p>
+                    <h3 className="text-xl text-white">Leerjaar</h3>
+                    <p className='mb-2'>2</p>
+                    <h3 className="text-xl text-white">Vaardighede</h3>
+                    <div className='mb-2'>
+                        <span className="inline-block bg-pink-300 text-black px-3 py-1 rounded-full text-sm font-medium mr-2 mb-2">Python</span>
+                        <span className="inline-block bg-pink-300 text-black px-3 py-1 rounded-full text-sm font-medium mr-2 mb-2">C#</span>
+                    </div>
+                    <h3 className="text-xl text-white">Intresses</h3>
+                    <div className='mb-4'>
+                        <span className="inline-block bg-blue-300 text-black px-3 py-1 rounded-full text-sm font-medium mr-2 mb-2">AI</span>
+                        <span className="inline-block bg-blue-300 text-black px-3 py-1 rounded-full text-sm font-medium mr-2 mb-2">Web Development</span>
+                    </div>
+                    <button
+                        type="button"
+                        onClick={() => navigate('/profile/createProfile')}
+                        className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+                    >
+                        Aanpassen
+                    </button>
+                </div>
                 <div className="bg-gray-800 rounded-lg p-6">
                     <h2 className="text-2xl font-bold text-white mb-6 text-center">
                         {t.dashboard.favoriteModules}
